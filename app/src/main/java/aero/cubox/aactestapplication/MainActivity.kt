aa49b50b4f6f37fd6af4e3e7a166e39eca2d5984
swon.cubox <swon.cubox@gmail.com>
2021-02-25 16:40:24 +0900

    basic Room and Livedata example

--- app/src/main/java/aero/cubox/aactestapplication/MainActivity.kt
+++ app/src/main/java/aero/cubox/aactestapplication/MainActivity.kt
@@ -2,10 +2,29 @@ package aero.cubox.aactestapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).allowMainThreadQueries().build()
+
+
+//        txt_result.text = db.todoDao().getAll().toString()
+        db.todoDao().getAll().observe(this, Observer {
+            txt_result.text = it.toString()
+        })
+
+        btn_add.setOnClickListener{
+            db.todoDao().insert(Todo(edt_todo.text.toString()))
+//            txt_result.text = db.todoDao().getAll().toString()
+        }
     }
 }
